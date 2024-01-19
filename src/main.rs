239031e9f6b2c293a6f277f9a92f362f0bbc6cc7
cladur <cl4dur@gmail.com>
2024-01-19 17:10:49 +0100

    feat: let language

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,29 @@
-use std::io::Write;
-
-use parser::{parse, tokenize, visualize, Expression, Token};
+use parser::{parse, tokenize};
 
 mod parser;
 
-/// Partially evaluate the L_int language.
-fn partially_evaluate(exp: Expression) -> Expression {
-    match exp {
-        Expression::Number(n) => Expression::Number(n),
-        Expression::Call(id, args) => match id.as_str() {
-            "+" => {
-                let mut sum = 0;
-                let mut new_args = Vec::new();
-                for arg in args.iter() {
-                    match partially_evaluate(arg.clone()) {
-                        Expression::Number(n) => sum += n,
-                        Expression::Call(id, args) => {
-                            new_args.push(Expression::Call(id, args));
-                        }
-                    }
-                }
-                if new_args.is_empty() {
-                    Expression::Number(sum)
-                } else {
-                    new_args.insert(0, Expression::Number(sum));
-                    Expression::Call("+".to_string(), new_args)
-                }
-            }
-            "-" => {
-                let mut new_args = Vec::new();
-                let minuend = partially_evaluate(args[0].clone());
-                let subtrahends = &args[1..];
-                let mut subtrahend_sum = 0;
-                for subtrahend in subtrahends.iter() {
-                    match partially_evaluate(subtrahend.clone()) {
-                        Expression::Number(n) => subtrahend_sum += n,
-                        Expression::Call(id, args) => {
-                            new_args.push(Expression::Call(id, args));
-                        }
-                    }
-                }
-                match minuend {
-                    Expression::Number(n) => {
-                        if new_args.is_empty() {
-                            Expression::Number(n - subtrahend_sum)
-                        } else {
-                            new_args.insert(0, Expression::Number(n - subtrahend_sum));
-                            Expression::Call("-".to_string(), new_args)
-                        }
-                    }
-                    Expression::Call(id, args) => {
-                        new_args.insert(0, Expression::Call(id, args));
-                        new_args.push(Expression::Number(subtrahend_sum));
-                        Expression::Call("-".to_string(), new_args)
-                    }
-                }
-            }
-            "read" => {
-                // If we have a read, we can't partially evaluate it
-                Expression::Call(id, args)
-            }
-            _ => {
-                // If we have an unknown function, we can't partially evaluate it
-                Expression::Call(id, args)
-            }
-        },
-    }
-}
-
-/// Interpret the L_int language.
-fn interpret(exp: Expression) -> i32 {
-    match exp {
-        Expression::Number(n) => n,
-        Expression::Call(id, args) => match id.as_str() {
-            "+" => {
-                let mut sum = 0;
-                for arg in args {
-                    sum += interpret(arg);
-                }
-                sum
-            }
-            "-" => {
-                let mut sum = interpret(args[0].clone());
-                for arg in args[1..].iter() {
-                    sum -= interpret(arg.clone());
-                }
-                sum
-            }
-            "read" => {
-                let mut input = String::new();
-                print!("> ");
-                // flush
-                std::io::stdout().flush().unwrap();
-                std::io::stdin().read_line(&mut input).unwrap();
-                input.trim().parse().unwrap()
-            }
-            _ => {
-                0 // Placeholder value, replace with appropriate code
-            }
-        },
-    }
-}
-
 fn main() {
-    let input = "(+ 20 1 (read) 15 (- 10 3))";
+    let input = "(let ((x 2) (y 3)) (let ((z (+ x y))) (+ z (read))))";
+
+    // Check if parens are balanced
+    let mut parens = 0;
+    for c in input.chars() {
+        if c == '(' {
+            parens += 1;
+        } else if c == ')' {
+            parens -= 1;
+        }
+    }
+    if parens != 0 {
+        println!("Unbalanced parens!!!");
+    }
+
     let tokens = tokenize(input);
     let ast = parse(&tokens);
 
-    println!("AST: {}", visualize(&ast));
+    println!("AST: {}", &ast);
 
-    let pe_ast = partially_evaluate(ast.clone());
+    // println!("Partially evaluated AST: {}", visualize(&pe_ast));
 
-    println!("Partially evaluated AST: {}", visualize(&pe_ast));
-
-    println!("{}", interpret(ast));
-    println!("{}", interpret(pe_ast));
+    println!("{}", ast.evaluate());
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 #[derive(Debug, PartialEq)]
 pub enum Token {
     Identifier(String),
@@ -8,10 +10,32 @@ pub enum Token {
 }
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum Expression {
+pub enum LInt {
     Number(i32),
-    Call(String, Vec<Expression>),
+    Add(Box<Expression>, Box<Expression>),
+    Subtract(Box<Expression>, Box<Expression>),
+    Read(),
 }
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Binding {
+    pub name: String,
+    pub value: Expression,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum LVar {
+    Let(Vec<Binding>, Box<Expression>),
+    Var(String),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Expression {
+    LInt(LInt),
+    LVar(LVar),
+}
+
+pub type Env = std::collections::HashMap<String, i32>;
 
 /// Takes a string and returns a vector of tokens.
 pub fn tokenize(input: &str) -> Vec<Token> {
@@ -74,63 +98,168 @@ pub fn parse(tokens: &[Token]) -> Expression {
 fn parse_expression(tokens: &mut std::iter::Peekable<std::slice::Iter<'_, Token>>) -> Expression {
     match tokens.peek() {
         Some(&Token::Number(_)) => parse_number(tokens),
-        Some(&Token::LParen) => parse_call(tokens),
+        Some(&Token::Identifier(_)) => parse_identifier(tokens),
+        Some(&Token::LParen) => {
+            assert!(tokens.next() == Some(&&Token::LParen));
+
+            let tok = tokens.next().unwrap();
+            let mut args = Vec::new();
+
+            if *tok == Token::Identifier("let".to_string()) {
+                let mut bindings = Vec::new();
+
+                // Skip LParen
+                assert!(tokens.next() == Some(&&Token::LParen));
+
+                // Parse bindings
+                while tokens.peek() != Some(&&Token::RParen) {
+                    // Skip LParen
+                    assert!(tokens.next() == Some(&&Token::LParen));
+
+                    let name = match tokens.next() {
+                        Some(Token::Identifier(id)) => id,
+                        _ => panic!("Expected identifier"),
+                    };
+                    let value = parse_expression(tokens);
+
+                    // Skip RParen
+                    assert!(tokens.next() == Some(&&Token::RParen));
+
+                    bindings.push(Binding {
+                        name: name.to_string(),
+                        value,
+                    });
+                }
+
+                // Skip RParen
+                assert!(tokens.next() == Some(&&Token::RParen));
+
+                return Expression::LVar(LVar::Let(bindings, Box::new(parse_expression(tokens))));
+            }
+
+            while tokens.peek() != Some(&&Token::RParen) {
+                args.push(parse_expression(tokens));
+            }
+            assert!(tokens.next() == Some(&&Token::RParen));
+            match tok {
+                Token::Identifier(id) => match id.as_str() {
+                    "+" => Expression::LInt(LInt::Add(
+                        Box::new(args[0].clone()),
+                        Box::new(args[1].clone()),
+                    )),
+                    "-" => Expression::LInt(LInt::Subtract(
+                        Box::new(args[0].clone()),
+                        Box::new(args[1].clone()),
+                    )),
+                    "read" => Expression::LInt(LInt::Read()),
+                    _ => panic!("Unexpected token"),
+                },
+                _ => panic!("Unexpected token"),
+            }
+        }
+        Some(&Token::RParen) => panic!("Unexpected RParen"),
         _ => panic!("Unexpected token"),
     }
 }
 
 fn parse_number(tokens: &mut std::iter::Peekable<std::slice::Iter<'_, Token>>) -> Expression {
     match tokens.next() {
-        Some(Token::Number(n)) => Expression::Number(*n),
+        Some(Token::Number(n)) => Expression::LInt(LInt::Number(*n)),
         _ => panic!("Expected number"),
     }
 }
 
-fn parse_call(tokens: &mut std::iter::Peekable<std::slice::Iter<'_, Token>>) -> Expression {
+fn parse_identifier(tokens: &mut std::iter::Peekable<std::slice::Iter<'_, Token>>) -> Expression {
     match tokens.next() {
-        Some(Token::LParen) => (),
-        _ => panic!("Expected '('"),
-    }
-
-    let id = match tokens.next() {
-        Some(Token::Identifier(id)) => id.to_string(),
+        Some(Token::Identifier(id)) => Expression::LVar(LVar::Var(id.to_string())),
         _ => panic!("Expected identifier"),
-    };
-
-    let mut args = Vec::new();
-
-    loop {
-        match tokens.peek() {
-            Some(Token::RParen) => break,
-            Some(_) => args.push(parse_expression(tokens)),
-            None => panic!("Expected ')'"),
-        }
     }
-
-    match tokens.next() {
-        Some(Token::RParen) => (),
-        _ => panic!("Expected ')'"),
-    }
-
-    Expression::Call(id, args)
 }
 
-pub fn visualize(exp: &Expression) -> String {
-    match exp {
-        Expression::Number(n) => n.to_string(),
-        Expression::Call(id, args) => {
-            let mut result = format!("({}", id);
-            if !args.is_empty() {
-                result += " ";
+pub trait Eval {
+    fn eval(&self, env: &mut Env) -> i32;
+}
+
+impl Eval for LInt {
+    fn eval(&self, env: &mut Env) -> i32 {
+        match self {
+            LInt::Number(n) => *n,
+            LInt::Add(a, b) => a.eval(env) + b.eval(env),
+            LInt::Subtract(a, b) => a.eval(env) - b.eval(env),
+            LInt::Read() => {
+                let mut input = String::new();
+                print!("> ");
+                // flush
+                std::io::stdout().flush().unwrap();
+                std::io::stdin().read_line(&mut input).unwrap();
+                input.trim().parse().unwrap()
             }
-            for (i, arg) in args.iter().enumerate() {
-                result += &visualize(arg);
-                if i < args.len() - 1 {
-                    result += " ";
+        }
+    }
+}
+
+impl Eval for LVar {
+    fn eval(&self, env: &mut Env) -> i32 {
+        match self {
+            LVar::Let(bindings, body) => {
+                for binding in bindings {
+                    let value = binding.value.eval(env);
+                    env.insert(binding.name.clone(), value);
                 }
+                body.eval(env)
             }
-            result += ")";
-            result
+            LVar::Var(name) => env.get(name).unwrap().clone(),
+        }
+    }
+}
+
+impl Expression {
+    pub fn evaluate(&self) -> i32 {
+        let mut env = Env::new();
+        self.eval(&mut env)
+    }
+}
+
+impl Eval for Expression {
+    fn eval(&self, env: &mut Env) -> i32 {
+        match self {
+            Expression::LInt(lint) => lint.eval(env),
+            Expression::LVar(lvar) => lvar.eval(env),
+        }
+    }
+}
+
+impl std::fmt::Display for Expression {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Expression::LInt(lint) => write!(f, "{}", lint),
+            Expression::LVar(lvar) => write!(f, "{}", lvar),
+        }
+    }
+}
+
+impl std::fmt::Display for LInt {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            LInt::Number(n) => write!(f, "{}", n),
+            LInt::Add(a, b) => write!(f, "(+ {} {})", a, b),
+            LInt::Subtract(a, b) => write!(f, "(- {} {})", a, b),
+            LInt::Read() => write!(f, "(read)"),
+        }
+    }
+}
+
+impl std::fmt::Display for LVar {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            LVar::Let(bindings, body) => {
+                write!(f, "(let (")?;
+                for binding in bindings {
+                    write!(f, "({} {})", binding.name, binding.value)?;
+                }
+                write!(f, ") {})", body)
+            }
+            LVar::Var(name) => write!(f, "{}", name),
         }
     }
 }
@@ -157,15 +286,37 @@ mod tests {
 
         assert_eq!(tokenize(input), expected);
 
-        let input = "(+ 2 (- 4 2))";
+        let input = "(let ((x 2) (y 3)) (let ((z (+ x y))) (+ z 1)))";
         let expected = vec![
             Token::LParen,
-            Token::Identifier("+".to_string()),
-            Token::Number(2),
+            Token::Identifier("let".to_string()),
             Token::LParen,
-            Token::Identifier("-".to_string()),
-            Token::Number(4),
+            Token::LParen,
+            Token::Identifier("x".to_string()),
             Token::Number(2),
+            Token::RParen,
+            Token::LParen,
+            Token::Identifier("y".to_string()),
+            Token::Number(3),
+            Token::RParen,
+            Token::RParen,
+            Token::LParen,
+            Token::Identifier("let".to_string()),
+            Token::LParen,
+            Token::LParen,
+            Token::Identifier("z".to_string()),
+            Token::LParen,
+            Token::Identifier("+".to_string()),
+            Token::Identifier("x".to_string()),
+            Token::Identifier("y".to_string()),
+            Token::RParen,
+            Token::RParen,
+            Token::RParen,
+            Token::LParen,
+            Token::Identifier("+".to_string()),
+            Token::Identifier("z".to_string()),
+            Token::Number(1),
+            Token::RParen,
             Token::RParen,
             Token::RParen,
             Token::EndOfFile,
@@ -176,38 +327,52 @@ mod tests {
 
     #[test]
     fn test_parse() {
-        let input = "(add 2 (subtract 4 2))";
+        let input = "(+ 2 (- 4 2))";
         let tokens = tokenize(input);
-        let expected = Expression::Call(
-            "add".to_string(),
-            vec![
-                Expression::Number(2),
-                Expression::Call(
-                    "subtract".to_string(),
-                    vec![Expression::Number(4), Expression::Number(2)],
-                ),
-            ],
-        );
+        let expected = Expression::LInt(LInt::Add(
+            Box::new(Expression::LInt(LInt::Number(2))),
+            Box::new(Expression::LInt(LInt::Subtract(
+                Box::new(Expression::LInt(LInt::Number(4))),
+                Box::new(Expression::LInt(LInt::Number(2))),
+            ))),
+        ));
 
         assert_eq!(parse(&tokens), expected);
 
-        let input = "(- 100 (+ 3 4 (read)))";
-        let tokens = tokenize(input);
-        let expected = Expression::Call(
-            "-".to_string(),
-            vec![
-                Expression::Number(100),
-                Expression::Call(
-                    "+".to_string(),
-                    vec![
-                        Expression::Number(3),
-                        Expression::Number(4),
-                        Expression::Call("read".to_string(), vec![]),
-                    ],
-                ),
-            ],
-        );
+        // let input = "(- 100 (+ 3 4 (read)))";
+        // let tokens = tokenize(input);
+        // let expected = Expression::Call(
+        //     "-".to_string(),
+        //     vec![
+        //         Expression::Number(100),
+        //         Expression::Call(
+        //             "+".to_string(),
+        //             vec![
+        //                 Expression::Number(3),
+        //                 Expression::Number(4),
+        //                 Expression::Call("read".to_string(), vec![]),
+        //             ],
+        //         ),
+        //     ],
+        // );
 
-        assert_eq!(parse(&tokens), expected);
+        // assert_eq!(parse(&tokens), expected);
+    }
+
+    #[test]
+    fn test_eval() {
+        let input = "(+ 2 (- 4 2))";
+        let tokens = tokenize(input);
+        let ast = parse(&tokens);
+        let expected = 4;
+
+        assert_eq!(ast.evaluate(), expected);
+
+        // let input = "(- 100 (+ 3 4 (read)))";
+        // let tokens = tokenize(input);
+        // let ast = parse(&tokens);
+        // let expected = 89;
+
+        // assert_eq!(ast.eval(), expected);
     }
 }
